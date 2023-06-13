268b1aafa5d94bf0d8720a494063da064934fa08
Nicholas Davolt <nicholadavolt@outlook.com>
2023-06-13 15:09:10 -0500

    Scaffolded GuitarShop tables

---
+++ StoreFront.DATA.EF/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StoreFront.DATA.EF.Models
+{
+    public partial class Product
+    {
+        public Product()
+        {
+            OrderProducts = new HashSet<OrderProduct>();
+        }
+
+        public int Id { get; set; }
+        public string ProductName { get; set; } = null!;
+        public string? ProductDesc { get; set; }
+        public short? UnitsInStock { get; set; }
+        public decimal? SellPrice { get; set; }
+        public decimal? PurchasePrice { get; set; }
+        public int TypeId { get; set; }
+        public int StatusId { get; set; }
+        public int? BuilderId { get; set; }
+        public string? ProductImage { get; set; }
+
+        public virtual Builder? Builder { get; set; }
+        public virtual Status Status { get; set; } = null!;
+        public virtual Type Type { get; set; } = null!;
+        public virtual ICollection<OrderProduct> OrderProducts { get; set; }
+    }
+}
---
+++ StoreFront.DATA.EF/Models/Status.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StoreFront.DATA.EF.Models
+{
+    public partial class Status
+    {
+        public Status()
+        {
+            Products = new HashSet<Product>();
+        }
+
+        public int Id { get; set; }
+        public string? Status1 { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
