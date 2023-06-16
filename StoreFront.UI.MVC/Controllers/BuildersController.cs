e387df6afd64798fbdf8980cd4a4fdeea235ab20
Nicholas Davolt <nicholadavolt@outlook.com>
2023-06-16 08:08:58 -0500

    Scaffolding views and template changes.

---
+++ StoreFront.UI.MVC/Controllers/BuildersController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using StoreFront.DATA.EF.Models;
+
+namespace StoreFront.UI.MVC.Controllers
+{
+    public class BuildersController : Controller
+    {
+        private readonly GuitarShopContext _context;
+
+        public BuildersController(GuitarShopContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Builders
+        public async Task<IActionResult> Index()
+        {
+              return _context.Builders != null ? 
+                          View(await _context.Builders.ToListAsync()) :
+                          Problem("Entity set 'GuitarShopContext.Builders'  is null.");
+        }
+
+        // GET: Builders/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Builders == null)
+            {
+                return NotFound();
+            }
+
+            var builder = await _context.Builders
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (builder == null)
+            {
+                return NotFound();
+            }
+
+            return View(builder);
+        }
+
+        // GET: Builders/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Builders/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Builder1")] Builder builder)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(builder);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(builder);
+        }
+
+        // GET: Builders/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Builders == null)
+            {
+                return NotFound();
+            }
+
+            var builder = await _context.Builders.FindAsync(id);
+            if (builder == null)
+            {
+                return NotFound();
+            }
+            return View(builder);
+        }
+
+        // POST: Builders/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Builder1")] Builder builder)
+        {
+            if (id != builder.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(builder);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BuilderExists(builder.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(builder);
+        }
+
+        // GET: Builders/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Builders == null)
+            {
+                return NotFound();
+            }
+
+            var builder = await _context.Builders
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (builder == null)
+            {
+                return NotFound();
+            }
+
+            return View(builder);
+        }
+
+        // POST: Builders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Builders == null)
+            {
+                return Problem("Entity set 'GuitarShopContext.Builders'  is null.");
+            }
+            var builder = await _context.Builders.FindAsync(id);
+            if (builder != null)
+            {
+                _context.Builders.Remove(builder);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BuilderExists(int id)
+        {
+          return (_context.Builders?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ StoreFront.UI.MVC/Controllers/StatusController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using StoreFront.DATA.EF.Models;
+
+namespace StoreFront.UI.MVC.Controllers
+{
+    public class StatusController : Controller
+    {
+        private readonly GuitarShopContext _context;
+
+        public StatusController(GuitarShopContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Status
+        public async Task<IActionResult> Index()
+        {
+              return _context.Statuses != null ? 
+                          View(await _context.Statuses.ToListAsync()) :
+                          Problem("Entity set 'GuitarShopContext.Statuses'  is null.");
+        }
+
+        // GET: Status/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Statuses == null)
+            {
+                return NotFound();
+            }
+
+            var status = await _context.Statuses
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (status == null)
+            {
+                return NotFound();
+            }
+
+            return View(status);
+        }
+
+        // GET: Status/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Status/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Status1")] Status status)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(status);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(status);
+        }
+
+        // GET: Status/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Statuses == null)
+            {
+                return NotFound();
+            }
+
+            var status = await _context.Statuses.FindAsync(id);
+            if (status == null)
+            {
+                return NotFound();
+            }
+            return View(status);
+        }
+
+        // POST: Status/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Status1")] Status status)
+        {
+            if (id != status.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(status);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StatusExists(status.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(status);
+        }
+
+        // GET: Status/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Statuses == null)
+            {
+                return NotFound();
+            }
+
+            var status = await _context.Statuses
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (status == null)
+            {
+                return NotFound();
+            }
+
+            return View(status);
+        }
+
+        // POST: Status/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Statuses == null)
+            {
+                return Problem("Entity set 'GuitarShopContext.Statuses'  is null.");
+            }
+            var status = await _context.Statuses.FindAsync(id);
+            if (status != null)
+            {
+                _context.Statuses.Remove(status);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool StatusExists(int id)
+        {
+          return (_context.Statuses?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
